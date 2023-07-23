947a16ac8a7f7e282dde8fbaf19ac3d01dfbabd5
Sanazzzmi <sanazzz246@gmail.com>
2023-07-23 10:31:27 +0330

    getting ip from command

--- vw-gateway/bin/check-attendance-zkteco.py
+++ vw-gateway/bin/check-attendance-zkteco.py
@@ -1,26 +1,31 @@
 from zk import ZK, const
+import sys
+import logging
 
-try:
-    _ip = input('Enter your ip:')
-    _password = input('Enter your password:')
-except EOFError as e:
-    print(e)
+_ip = sys.argv[1]
+_password = sys.argv[2]
+
+logging.basicConfig(level=logging.DEBUG)
+
+logging.debug("IP Address is %s from input", _ip)
+logging.debug("Password %s from input", _password)
     
 conn = None
-# create ZK instance
+logging.debug("create ZK instance")
 zk = ZK(str(_ip), port=4370, timeout=5, password=int(_password), force_udp=False, ommit_ping=False)
+
 try:
-    # connect to device
+    logging.debug("Connect to device")
     conn = zk.connect()
     # disable device, this method ensures no activity on the device while the process is run
     conn.disable_device()
     # another commands will be here!
     # Example: Get All Users
-    attendances = conn.get_attendance()
-    print(attendances)
+    users = conn.get_users()
+    print(users)
     
 except Exception as e:
-    print ("Process terminate : {}".format(e))
+    logging.error("Fail to get attendences", exc_info=True)
 finally:
     if conn:
         conn.disconnect()
