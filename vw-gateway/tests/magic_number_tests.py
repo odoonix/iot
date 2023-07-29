aacedf9d5ff48ae6f30855b305b48ef33f9e82b0
Sanazzzmi <sanazzz246@gmail.com>
2023-07-29 10:58:07 +0330

    add check-fingers

--- vw-gateway/tests/magic_number_tests.py
+++ vw-gateway/tests/magic_number_tests.py
@@ -9,21 +9,19 @@ class TestStringMethods(unittest.TestCase):
 
     def test_convert_to_device_id(self):
         data = [
-            (1, 0x7C01, 62),
+            (1, 31745, 62),
             (1, 0x3C01, 30),
             (1, 0x201,  1),
             (1, 513,  1),
             # Magic number is default
-            (0x1, 0x1, 0x0),
-            (0xFFF, 0xFFF, 0x0),
+            (513, 513, 0),
+            (4095, 4095, 0x0),
         ]
         for user_id, device_id, magic_number in data:
             self.assertEqual(zktec_connector.convert_to_device_id(magic_number, user_id),
                             device_id)
-            print(zktec_connector.convert_to_device_id(magic_number, user_id))
             self.assertEqual(zktec_connector.convert_to_company_id(magic_number, device_id),
                             user_id)
-            print(zktec_connector.convert_to_company_id(magic_number, device_id))
 
     def test_convert_to_device_id_invalid(self):
         ids = [
--- vw-gateway/tests/send_packet.py
+++
@@ -1,9 +0,0 @@
-import unittest
-import sys
-import os
-
-sys.path.append(os.path.abspath('/home/sanaz/viraweb123/odoo-iot/vw-gateway/extensions/attendence/zktec'))
-import zktec_connector
-
-class TestStringMethods(unittest.TestCase):
-    
