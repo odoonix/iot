5ab62195b7b5579725615c21d47967eeae45b37d
maso <mostafa.barmshory@gmail.com>
2025-05-31 20:23:33 +0330

    [UP] adding more commands

--- src/tb_gateway/zkteco/cli.py
+++ src/tb_gateway/zkteco/cli.py
@@ -18,11 +18,33 @@ def check(
     devices.zk_devices_check_connection(ip, password)
 
 
-@app.command()
-def list():
-    """List installed devices
+@app.command("check-utf")
+def check_utf(
+    ip: Annotated[str, typer.Option(help="IP address of the device.")],
+    password: Annotated[str, typer.Option(help="Devise access key.")],
+):
+    """Check if possible to write UTF8 text
     """
-    print("Not implemented")
+    devices.zk_devices_check_utf8(ip, password)
+
+
+@app.command("get-attendance")
+def _get_attendance(
+    ip: Annotated[str, typer.Option(help="IP address of the device.")],
+    password: Annotated[str, typer.Option(help="Devise access key.")],
+):
+    """Get attendances"""
+    devices.zk_devices_get_attendance(ip, password)
+
+
+@app.command("user-info")
+def _show_user_info_cmd(
+    ip: Annotated[str, typer.Option(help="IP address of the device.")],
+    password: Annotated[str, typer.Option(help="Devise access key.")],
+):
+    """Shows user infor"""
+    devices.zk_devices_get_user_info(ip, password)
+
 
 if __name__ == "__main__":
     app()
--- src/tb_gateway/zkteco/devices.py
+++ src/tb_gateway/zkteco/devices.py
@@ -6,12 +6,11 @@ import rich
 from zk import ZK, const
 
 
-def zk_devices_check_connection(ip=None, password=None):
+def zk_devices_check_connection(ip=None, password=None, port=4370):
     """Connects to the ZK device and get more information from device.
     """
     logging.debug("create ZK instance")
-    conn = None
-    zk = ZK(str(ip), port=4370, timeout=5, password=int(
+    zk = ZK(str(ip), port=port, timeout=5, password=int(
         password), force_udp=False, ommit_ping=True)
 
     try:
@@ -21,8 +20,87 @@ def zk_devices_check_connection(ip=None, password=None):
         users = conn.get_users()
         rich.print(users)
         rich.print(attendances)
-    except Exception as e:
+    finally:
+        if conn:
+            conn.disconnect()
+
+
+def zk_devices_check_utf8(ip=None, password=None, port=4370):
+    """Check devices language
+    """
+    conn = None
+    logging.debug("create ZK instance")
+    zk = ZK(str(ip), port=port, timeout=5, password=int(
+        password), force_udp=False, ommit_ping=True)
+
+    try:
+        logging.debug("Connect to device")
+        conn = zk.connect()
+        # disable device, this method ensures no activity on the device while the
+        # process is run
+        conn.disable_device()
+        # another commands will be here!
+        conn.set_user(uid=1000, name='فارسی', privilege=const.USER_DEFAULT,
+                      password='', group_id='', user_id='1000', card=0)
+
+    except Exception:
         logging.error("Fail to get attendences", exc_info=True)
+    finally:
+        if conn:
+            conn.disconnect()
+
+
+def zk_devices_get_attendance(ip=None, password=None, port=4370):
+    """Check devices language
+    """
+    logging.debug("create ZK instance")
+    zk = ZK(str(ip), port=port, timeout=5, password=int(
+        password), force_udp=False, ommit_ping=True)
+
+    try:
+        logging.debug("Connect to device")
+        conn = zk.connect()
+        # disable device, this method ensures no activity on the device while the process is run
+        conn.disable_device()
+        # another commands will be here!
+        # Example: Get All Users
+        attendances = conn.get_attendance()
+        rich.print(attendances)
+    finally:
+        if conn:
+            conn.disconnect()
+
+
+def zk_devices_get_user_info(ip=None, password=None, port=4370):
+    """Check devices language
+    """
+    logging.debug("create ZK instance")
+    zk = ZK(str(ip), port=port, timeout=5, password=int(
+        password), force_udp=False, ommit_ping=True)
+
+    try:
+        logging.debug("Connect to device")
+        conn = zk.connect()
+        # disable device, this method ensures no activity on the device while the process is run
+        conn.disable_device()
+        # another commands will be here!
+        # Example: Get All Users
+        users = conn.get_users()
+        for user in users:
+            privilege = 'User'
+            if user.privilege == const.USER_ADMIN:
+                privilege = 'Admin'
+            rich.print('+ UID #{}'.format(user.uid))
+            rich.print('  Name       : {}'.format(user.name))
+            rich.print('  Privilege  : {}'.format(privilege))
+            rich.print('  Password   : {}'.format(user.password))
+            rich.print('  Group ID   : {}'.format(user.group_id))
+            rich.print('  User  ID   : {}'.format(user.user_id))
+
+        # Test Voice: Say Thank You
+        conn.test_voice()
+        # re-enable device after all commands already executed
+        conn.enable_device()
     finally:
         if conn:
             conn.disconnect()
